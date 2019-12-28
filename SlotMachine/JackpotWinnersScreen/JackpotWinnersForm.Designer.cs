b7c1a1f952ec641b79af11b990598c7e18b4cfc1
Spookyi <sergiubutu99@gmail.com>
2019-12-28 20:14:43 +0300

    jackpotbois v1

---
+++ SlotMachine/JackpotWinnersScreen/JackpotWinnersForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace JackpotWinnersScreen
+{
+    partial class JackpotWinnersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JackpotWinnersForm));
+            this.backgroundImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backgroundImage
+            // 
+            this.backgroundImage.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("backgroundImage.BackgroundImage")));
+            this.backgroundImage.Location = new System.Drawing.Point(0, 0);
+            this.backgroundImage.Name = "backgroundImage";
+            this.backgroundImage.Size = new System.Drawing.Size(100, 50);
+            this.backgroundImage.TabIndex = 0;
+            this.backgroundImage.TabStop = false;
+            // 
+            // JackpotWinnersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.backgroundImage);
+            this.Name = "JackpotWinnersForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.backgroundImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox backgroundImage;
+    }
+}
+
