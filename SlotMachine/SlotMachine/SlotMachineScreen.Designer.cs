2d226b2b2f1d3cb7691032ede60b84d99fa7a75a
Spookyi <sergiubutu99@gmail.com>
2020-01-02 15:04:56 +0200

    no message

---
+++ SlotMachine/SlotMachine/SlotMachineScreen.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SlotMachine {
+    partial class SlotMachineScreen {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+            xmlReader.updateJackpot(jackpotChance);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.backButton = new System.Windows.Forms.Button();
+            this.SlotsColumns = new System.Windows.Forms.PictureBox();
+            this.BackgroundImage = new System.Windows.Forms.PictureBox();
+            this.spinButton = new System.Windows.Forms.Button();
+            this.moreBet = new System.Windows.Forms.Button();
+            this.lessBet = new System.Windows.Forms.Button();
+            this.gambleButton = new System.Windows.Forms.Button();
+            this.BetLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.SlotsColumns)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(535, 337);
+            this.backButton.Margin = new System.Windows.Forms.Padding(2);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(56, 19);
+            this.backButton.TabIndex = 2;
+            this.backButton.Text = "button1";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // SlotsColumns
+            // 
+            this.SlotsColumns.Image = global::SlotMachine.Properties.Resources.sluts;
+            this.SlotsColumns.Location = new System.Drawing.Point(6, 6);
+            this.SlotsColumns.Margin = new System.Windows.Forms.Padding(2);
+            this.SlotsColumns.Name = "SlotsColumns";
+            this.SlotsColumns.Size = new System.Drawing.Size(83, 81);
+            this.SlotsColumns.TabIndex = 1;
+            this.SlotsColumns.TabStop = false;
+            // 
+            // BackgroundImage
+            // 
+            this.BackgroundImage.Image = global::SlotMachine.Properties.Resources.background_image;
+            this.BackgroundImage.Location = new System.Drawing.Point(0, 0);
+            this.BackgroundImage.Margin = new System.Windows.Forms.Padding(2);
+            this.BackgroundImage.Name = "BackgroundImage";
+            this.BackgroundImage.Size = new System.Drawing.Size(602, 370);
+            this.BackgroundImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.BackgroundImage.TabIndex = 0;
+            this.BackgroundImage.TabStop = false;
+            // 
+            // spinButton
+            // 
+            this.spinButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.spinButton.AutoSize = true;
+            this.spinButton.BackColor = System.Drawing.Color.Red;
+            this.spinButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.spinButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.spinButton.Location = new System.Drawing.Point(256, 313);
+            this.spinButton.Margin = new System.Windows.Forms.Padding(2);
+            this.spinButton.Name = "spinButton";
+            this.spinButton.Size = new System.Drawing.Size(112, 43);
+            this.spinButton.TabIndex = 3;
+            this.spinButton.Text = "SPIN";
+            this.spinButton.UseVisualStyleBackColor = false;
+            this.spinButton.Click += new System.EventHandler(this.spinButton_Click);
+            // 
+            // moreBet
+            // 
+            this.moreBet.Location = new System.Drawing.Point(116, 340);
+            this.moreBet.Margin = new System.Windows.Forms.Padding(2);
+            this.moreBet.Name = "moreBet";
+            this.moreBet.Size = new System.Drawing.Size(56, 19);
+            this.moreBet.TabIndex = 4;
+            this.moreBet.Text = "button1";
+            this.moreBet.UseVisualStyleBackColor = true;
+            // 
+            // lessBet
+            // 
+            this.lessBet.Location = new System.Drawing.Point(9, 337);
+            this.lessBet.Margin = new System.Windows.Forms.Padding(2);
+            this.lessBet.Name = "lessBet";
+            this.lessBet.Size = new System.Drawing.Size(56, 19);
+            this.lessBet.TabIndex = 5;
+            this.lessBet.Text = "button1";
+            this.lessBet.UseVisualStyleBackColor = true;
+            // 
+            // gambleButton
+            // 
+            this.gambleButton.Location = new System.Drawing.Point(416, 337);
+            this.gambleButton.Margin = new System.Windows.Forms.Padding(2);
+            this.gambleButton.Name = "gambleButton";
+            this.gambleButton.Size = new System.Drawing.Size(56, 19);
+            this.gambleButton.TabIndex = 6;
+            this.gambleButton.Text = "button1";
+            this.gambleButton.UseVisualStyleBackColor = true;
+            // 
+            // BetLabel
+            // 
+            this.BetLabel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BetLabel.AutoSize = true;
+            this.BetLabel.Location = new System.Drawing.Point(70, 342);
+            this.BetLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.BetLabel.Name = "BetLabel";
+            this.BetLabel.Size = new System.Drawing.Size(25, 13);
+            this.BetLabel.TabIndex = 7;
+            this.BetLabel.Text = "aaa";
+            // 
+            // SlotMachineScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 366);
+            this.Controls.Add(this.BetLabel);
+            this.Controls.Add(this.gambleButton);
+            this.Controls.Add(this.lessBet);
+            this.Controls.Add(this.moreBet);
+            this.Controls.Add(this.spinButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.SlotsColumns);
+            this.Controls.Add(this.BackgroundImage);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "SlotMachineScreen";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.SlotsColumns)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BackgroundImage;
+        private System.Windows.Forms.PictureBox SlotsColumns;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button spinButton;
+        private System.Windows.Forms.Button moreBet;
+        private System.Windows.Forms.Button lessBet;
+        private System.Windows.Forms.Button gambleButton;
+        private System.Windows.Forms.Label BetLabel;
+    }
+}
+
---
+++ SlotMachine/SlotMachine/SlotMachineScreen.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Diagnostics;
+using System.Drawing.Text;
+using System.Runtime.InteropServices;
+using System.Threading;
+namespace SlotMachine {
+    public partial class SlotMachineScreen : Form {
+        Assembly xml;
+        dynamic xmlReader;
+        Assembly winningsCalc;
+        dynamic winningsCalculator;
+        private int[] chances = new int[9];
+        private int jackpotChance;
+        PrivateFontCollection egyptFont;
+        CurrentPlayer currentPlayer;
+        WinningsCalculator.WinType[] winTypes;
+        Random random = new Random();
+        WinningsCalculator. PictureMap[,] pictureMatrix = new WinningsCalculator.PictureMap[3, 5];
+        public SlotMachineScreen() {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+
+
+            
+            InitializeComponent();
+            setupFont();
+            setupScreen();
+
+            winningsCalc = Assembly.Load("WinningsCalculator");
+            winningsCalculator = winningsCalc.CreateInstance("WinningsCalculator.WinningsCalculator");
+            
+            xml = Assembly.Load("XmlReader");
+            xmlReader = xml.CreateInstance("XmlReader.XmlReader");
+            chances = xmlReader.getChances();
+            
+            winTypes = new WinningsCalculator.WinType[15];
+            
+            for (int i = 0; i < 9; i++) {
+                Debug.WriteLine(chances[i]);
+            }
+           
+            jackpotChance = chances[8];
+            currentPlayer = SlotMachine.CurrentPlayer.getInstance();
+            setupMatrix();
+        }
+
+        private void setupScreen() {
+            BackgroundImage.Dock = DockStyle.Fill;
+            SlotsColumns.Parent = BackgroundImage;
+            SlotsColumns.Dock = DockStyle.Fill;
+            SlotsColumns.BackColor = Color.Transparent;
+
+            int x = this.Width / 2;
+            int y = this.Height / 2;
+
+            setupButton(backButton, "Back", x + 700, y + 430);
+            setupButton(lessBet, "BET -", x - 860, y + 430);
+            setupButton(moreBet, "BET +", lessBet.Location.X + 450, y + 430);
+            setupSpinButton(spinButton, "SPIN", x-100, y + 400);
+            setupButton(gambleButton, "GAMBLE", spinButton.Location.X + 350, y + 430);
+            //gambleButton.Enabled = false;
+            gambleButton.Hide();
+
+            setupLabel(BetLabel, "BET: 5000", 35, x - 650, y + 440);   
+        }
+
+        private void setupButton(Button button, String text, int x, int y) {
+            button.Font = new Font(egyptFont.Families[0], 30);
+            button.BackColor = Color.Orange;
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderColor = Color.Yellow;
+            button.Text = text;
+            button.Location = new Point(x, y);
+            button.Anchor = AnchorStyles.None;
+            button.Width = 180;
+            button.Height = 60;
+            button.UseCompatibleTextRendering = true;
+        }
+
+        private void setupSpinButton(Button button, String text, int x, int y) {
+            button.Font = new Font(egyptFont.Families[0], 50);
+            button.BackColor = Color.Red;
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderColor = Color.Red;
+            button.Text = text;
+            button.Location = new Point(x, y);
+            button.Anchor = AnchorStyles.None;
+            button.Width = 250;
+            button.Height = 100;
+            button.UseCompatibleTextRendering = true;
+        }
+        private void setupLabel(Label label, String text, int fontSize, int x, int y) {
+            label.Parent = SlotsColumns;
+            label.BackColor = Color.Orange;
+            label.Font = new Font(egyptFont.Families[0], fontSize);
+            label.Text = text;
+            label.UseCompatibleTextRendering = true;
+            label.Anchor = AnchorStyles.None;
+            label.Location = new Point(x, y);
+        }
+
+        private void setupFont() {
+            egyptFont = new PrivateFontCollection();
+            int fontLength = Properties.Resources.ISIS.Length;
+            byte[] fontData = Properties.Resources.ISIS;
+            System.IntPtr data = Marshal.AllocCoTaskMem(fontLength);
+            Marshal.Copy(fontData, 0, data, fontLength);
+            egyptFont.AddMemoryFont(data, fontLength);
+        }
+
+        private void backButton_Click(object sender, EventArgs e) {
+            this.Dispose();
+        }
+        private void createPic(int i,int j)
+        {
+            PictureBox pic = new PictureBox();
+            pic.Size = new Size(286, 286);
+            pic.Parent = SlotsColumns;
+            pic.BackColor = Color.Transparent;
+            int y = 25 + (286 * i);
+            int x = 200 + (310 * j);
+            pic.Location = new Point(x, y);
+            pictureMatrix[i, j].PictureBox = pic;
+            pictureMatrix[i, j].PictureBox.BackgroundImageLayout = ImageLayout.Zoom;
+        }
+        private int[] generatePic(int i,int j)
+        {
+            int[] checks=new int[2];
+            checks[0] = 0;
+            checks[1] = 0;
+            int rnd = random.Next(1, 100);
+            if (rnd < chances[0])
+            {
+                pictureMatrix[i,j].PictureBox.Image = SlotMachine.Properties.Resources.cherry;
+                pictureMatrix[i, j].ImageId = "cherry";
+            }
+            else if (rnd < (chances[0] + chances[1]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.orange;
+                pictureMatrix[i, j].ImageId = "orange";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.lemon;
+                pictureMatrix[i, j].ImageId = "lemon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.plum;
+                pictureMatrix[i, j].ImageId = "plum";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.grapes;
+                pictureMatrix[i, j].ImageId = "grapes";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.watermelon;
+                pictureMatrix[i, j].ImageId = "watermelon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5] + chances[6]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.star;
+                pictureMatrix[i, j].ImageId = "star";
+                checks[0] = 1;
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5] + chances[6] + chances[7]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.seven;
+                pictureMatrix[i, j].ImageId = "seven";
+                checks[1] = 1;
+            }
+            return checks;
+        }
+
+        private void generateNoSeven(int i, int j)
+        {
+            int rnd = random.Next(1, 95);
+            if (rnd < chances[0])
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.cherry;
+                pictureMatrix[i, j].ImageId = "cherry";
+            }
+            else if (rnd < (chances[0] + chances[1]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.orange;
+                pictureMatrix[i, j].ImageId = "orange";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.lemon;
+                pictureMatrix[i, j].ImageId = "lemon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.plum;
+                pictureMatrix[i, j].ImageId = "plum";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.grapes;
+                pictureMatrix[i, j].ImageId = "grapes";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.watermelon;
+                pictureMatrix[i, j].ImageId = "watermelon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5] + chances[6]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.star;
+                pictureMatrix[i, j].ImageId = "star";
+            }
+        }
+
+        private void generateNoStar(int i, int j)
+        {
+            int rnd = random.Next(1, 93);
+            if (rnd < chances[0])
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.cherry;
+                pictureMatrix[i, j].ImageId = "cherry";
+            }
+            else if (rnd < (chances[0] + chances[1]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.orange;
+                pictureMatrix[i, j].ImageId = "orange";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.lemon;
+                pictureMatrix[i, j].ImageId = "lemon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.plum;
+                pictureMatrix[i, j].ImageId = "plum";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.grapes;
+                pictureMatrix[i, j].ImageId = "grapes";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.watermelon;
+                pictureMatrix[i, j].ImageId = "watermelon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5] + chances[7]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.seven;
+                pictureMatrix[i, j].ImageId = "seven";
+            }
+        }
+
+        private void generateBasicPic(int i, int j)
+        {
+            int rnd = random.Next(1, 88);
+            if (rnd < chances[0])
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.cherry;
+                pictureMatrix[i, j].ImageId = "cherry";
+            }
+            else if (rnd < (chances[0] + chances[1]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.orange;
+                pictureMatrix[i, j].ImageId = "orange";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.lemon;
+                pictureMatrix[i, j].ImageId = "lemon";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.plum;
+                pictureMatrix[i, j].ImageId = "plum";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.grapes;
+                pictureMatrix[i, j].ImageId = "grapes";
+            }
+            else if (rnd < (chances[0] + chances[1] + chances[2] + chances[3] + chances[4] + chances[5]))
+            {
+                pictureMatrix[i, j].PictureBox.Image = SlotMachine.Properties.Resources.watermelon;
+                pictureMatrix[i, j].ImageId = "watermelon";
+            }
+        }
+
+        private void setupMatrix()
+        {
+
+            for(int i = 0; i < 3; ++i)
+            {
+                for(int j = 0; j < 5; ++j)
+                {
+                    pictureMatrix[i, j] = new WinningsCalculator.PictureMap();
+                }
+            }
+            for(int j = 0; j < 5; j++)
+            {
+                int[] checks = new int[2];
+                checks[0] = 0;
+                checks[1] = 0;
+                for(int i = 0; i < 3; i++)
+                {
+                    createPic(i, j);
+                    if (checks[0] == 0 && checks[1] == 0)
+                    {
+                        checks = generatePic(i, j);
+                    }
+                    else if(checks[0]==1)
+                    {
+                        generateNoStar(i, j);
+                    }
+                    else if (checks[1] == 1)
+                    {
+                        generateNoSeven(i, j);
+                    }
+                    else
+                    {
+                        generateBasicPic(i, j);
+                    }
+                }
+            }
+        }
+        private void updateMatrix()
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                int[] checks = new int[2];
+                checks[0] = 0;
+                checks[1] = 0;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (checks[0] == 0 && checks[1] == 0)
+                    {
+                        checks = generatePic(i, j);
+                    }
+                    else if (checks[0] == 1)
+                    {
+                        generateNoStar(i, j);
+                    }
+                    else if (checks[1] == 1)
+                    {
+                        generateNoSeven(i, j);
+                    }
+                    else
+                    {
+                        generateBasicPic(i, j);
+                    }
+                }
+            }
+        }
+
+        private void spinButton_Click(object sender, EventArgs e)
+        {
+            updateMatrix();
+            String s="";
+            winTypes = winningsCalculator.findWins(pictureMatrix,winTypes);
+            for (int i = 0; i < 15; i++)
+            {
+                if (winTypes[i] != null)
+                {
+                    s+=winTypes[i].WinLine+" "+winTypes[i].IconAmount.ToString()+"\n";
+                }
+            }
+            MessageBox.Show(s);
+        }
+    }
+}
