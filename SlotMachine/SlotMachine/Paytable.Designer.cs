5ee7f7cb4a5cd2bcc2426253a0ecd81e5dd3e513
Adrian Cimpean <cimpean_adrian12@yahoo.com>
2020-01-03 22:35:46 +0200

    working project

---
+++ SlotMachine/SlotMachine/Paytable.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace SlotMachine {
+    partial class Paytable {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.BackgroundImage = new System.Windows.Forms.PictureBox();
+            this.BackButton = new System.Windows.Forms.Button();
+            this.Watermelon = new System.Windows.Forms.PictureBox();
+            this.Plum = new System.Windows.Forms.PictureBox();
+            this.Lemon = new System.Windows.Forms.PictureBox();
+            this.Grapes = new System.Windows.Forms.PictureBox();
+            this.Orange = new System.Windows.Forms.PictureBox();
+            this.Cherry = new System.Windows.Forms.PictureBox();
+            this.Seven = new System.Windows.Forms.PictureBox();
+            this.Star = new System.Windows.Forms.PictureBox();
+            this.WatermelonLabel = new System.Windows.Forms.Label();
+            this.PlumLabel = new System.Windows.Forms.Label();
+            this.LemonLabel = new System.Windows.Forms.Label();
+            this.SevenLabel = new System.Windows.Forms.Label();
+            this.StarLabel = new System.Windows.Forms.Label();
+            this.GrapesLabel = new System.Windows.Forms.Label();
+            this.OrangeLabel = new System.Windows.Forms.Label();
+            this.CherryLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Watermelon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lemon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grapes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Orange)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cherry)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Seven)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BackgroundImage
+            // 
+            this.BackgroundImage.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BackgroundImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BackgroundImage.Image = global::SlotMachine.Properties.Resources.background_image;
+            this.BackgroundImage.Location = new System.Drawing.Point(0, 0);
+            this.BackgroundImage.Name = "BackgroundImage";
+            this.BackgroundImage.Size = new System.Drawing.Size(1170, 644);
+            this.BackgroundImage.TabIndex = 0;
+            this.BackgroundImage.TabStop = false;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Location = new System.Drawing.Point(0, 12);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(75, 23);
+            this.BackButton.TabIndex = 1;
+            this.BackButton.Text = "button1";
+            this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // Watermelon
+            // 
+            this.Watermelon.Image = global::SlotMachine.Properties.Resources.watermelon;
+            this.Watermelon.Location = new System.Drawing.Point(79, 56);
+            this.Watermelon.Name = "Watermelon";
+            this.Watermelon.Size = new System.Drawing.Size(134, 114);
+            this.Watermelon.TabIndex = 2;
+            this.Watermelon.TabStop = false;
+            // 
+            // Plum
+            // 
+            this.Plum.Image = global::SlotMachine.Properties.Resources.plum;
+            this.Plum.Location = new System.Drawing.Point(99, 322);
+            this.Plum.Name = "Plum";
+            this.Plum.Size = new System.Drawing.Size(114, 78);
+            this.Plum.TabIndex = 3;
+            this.Plum.TabStop = false;
+            // 
+            // Lemon
+            // 
+            this.Lemon.Image = global::SlotMachine.Properties.Resources.lemon;
+            this.Lemon.Location = new System.Drawing.Point(120, 554);
+            this.Lemon.Name = "Lemon";
+            this.Lemon.Size = new System.Drawing.Size(100, 50);
+            this.Lemon.TabIndex = 4;
+            this.Lemon.TabStop = false;
+            // 
+            // Grapes
+            // 
+            this.Grapes.Image = global::SlotMachine.Properties.Resources.grapes;
+            this.Grapes.Location = new System.Drawing.Point(954, 97);
+            this.Grapes.Name = "Grapes";
+            this.Grapes.Size = new System.Drawing.Size(100, 50);
+            this.Grapes.TabIndex = 5;
+            this.Grapes.TabStop = false;
+            // 
+            // Orange
+            // 
+            this.Orange.Image = global::SlotMachine.Properties.Resources.orange;
+            this.Orange.Location = new System.Drawing.Point(992, 310);
+            this.Orange.Name = "Orange";
+            this.Orange.Size = new System.Drawing.Size(100, 50);
+            this.Orange.TabIndex = 6;
+            this.Orange.TabStop = false;
+            // 
+            // Cherry
+            // 
+            this.Cherry.Image = global::SlotMachine.Properties.Resources.cherry;
+            this.Cherry.Location = new System.Drawing.Point(992, 554);
+            this.Cherry.Name = "Cherry";
+            this.Cherry.Size = new System.Drawing.Size(100, 50);
+            this.Cherry.TabIndex = 7;
+            this.Cherry.TabStop = false;
+            // 
+            // Seven
+            // 
+            this.Seven.Image = global::SlotMachine.Properties.Resources.seven;
+            this.Seven.Location = new System.Drawing.Point(604, 119);
+            this.Seven.Name = "Seven";
+            this.Seven.Size = new System.Drawing.Size(100, 50);
+            this.Seven.TabIndex = 8;
+            this.Seven.TabStop = false;
+            // 
+            // Star
+            // 
+            this.Star.Image = global::SlotMachine.Properties.Resources.star;
+            this.Star.Location = new System.Drawing.Point(604, 447);
+            this.Star.Name = "Star";
+            this.Star.Size = new System.Drawing.Size(100, 50);
+            this.Star.TabIndex = 9;
+            this.Star.TabStop = false;
+            // 
+            // WatermelonLabel
+            // 
+            this.WatermelonLabel.AutoSize = true;
+            this.WatermelonLabel.Location = new System.Drawing.Point(220, 97);
+            this.WatermelonLabel.Name = "WatermelonLabel";
+            this.WatermelonLabel.Size = new System.Drawing.Size(45, 16);
+            this.WatermelonLabel.TabIndex = 10;
+            this.WatermelonLabel.Text = "label1";
+            // 
+            // PlumLabel
+            // 
+            this.PlumLabel.AutoSize = true;
+            this.PlumLabel.Location = new System.Drawing.Point(220, 359);
+            this.PlumLabel.Name = "PlumLabel";
+            this.PlumLabel.Size = new System.Drawing.Size(45, 16);
+            this.PlumLabel.TabIndex = 11;
+            this.PlumLabel.Text = "label1";
+            // 
+            // LemonLabel
+            // 
+            this.LemonLabel.AutoSize = true;
+            this.LemonLabel.Location = new System.Drawing.Point(220, 575);
+            this.LemonLabel.Name = "LemonLabel";
+            this.LemonLabel.Size = new System.Drawing.Size(45, 16);
+            this.LemonLabel.TabIndex = 12;
+            this.LemonLabel.Text = "label1";
+            // 
+            // SevenLabel
+            // 
+            this.SevenLabel.AutoSize = true;
+            this.SevenLabel.Location = new System.Drawing.Point(640, 176);
+            this.SevenLabel.Name = "SevenLabel";
+            this.SevenLabel.Size = new System.Drawing.Size(45, 16);
+            this.SevenLabel.TabIndex = 13;
+            this.SevenLabel.Text = "label1";
+            // 
+            // StarLabel
+            // 
+            this.StarLabel.AutoSize = true;
+            this.StarLabel.Location = new System.Drawing.Point(643, 413);
+            this.StarLabel.Name = "StarLabel";
+            this.StarLabel.Size = new System.Drawing.Size(45, 16);
+            this.StarLabel.TabIndex = 14;
+            this.StarLabel.Text = "label1";
+            // 
+            // GrapesLabel
+            // 
+            this.GrapesLabel.AutoSize = true;
+            this.GrapesLabel.Location = new System.Drawing.Point(903, 109);
+            this.GrapesLabel.Name = "GrapesLabel";
+            this.GrapesLabel.Size = new System.Drawing.Size(45, 16);
+            this.GrapesLabel.TabIndex = 15;
+            this.GrapesLabel.Text = "label1";
+            // 
+            // OrangeLabel
+            // 
+            this.OrangeLabel.AutoSize = true;
+            this.OrangeLabel.Location = new System.Drawing.Point(941, 322);
+            this.OrangeLabel.Name = "OrangeLabel";
+            this.OrangeLabel.Size = new System.Drawing.Size(45, 16);
+            this.OrangeLabel.TabIndex = 16;
+            this.OrangeLabel.Text = "label1";
+            // 
+            // CherryLabel
+            // 
+            this.CherryLabel.AutoSize = true;
+            this.CherryLabel.Location = new System.Drawing.Point(941, 565);
+            this.CherryLabel.Name = "CherryLabel";
+            this.CherryLabel.Size = new System.Drawing.Size(45, 16);
+            this.CherryLabel.TabIndex = 17;
+            this.CherryLabel.Text = "label1";
+            // 
+            // Paytable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 644);
+            this.Controls.Add(this.CherryLabel);
+            this.Controls.Add(this.OrangeLabel);
+            this.Controls.Add(this.GrapesLabel);
+            this.Controls.Add(this.StarLabel);
+            this.Controls.Add(this.SevenLabel);
+            this.Controls.Add(this.LemonLabel);
+            this.Controls.Add(this.PlumLabel);
+            this.Controls.Add(this.WatermelonLabel);
+            this.Controls.Add(this.Star);
+            this.Controls.Add(this.Seven);
+            this.Controls.Add(this.Cherry);
+            this.Controls.Add(this.Orange);
+            this.Controls.Add(this.Grapes);
+            this.Controls.Add(this.Lemon);
+            this.Controls.Add(this.Plum);
+            this.Controls.Add(this.Watermelon);
+            this.Controls.Add(this.BackButton);
+            this.Controls.Add(this.BackgroundImage);
+            this.Name = "Paytable";
+            this.Text = "Paytable";
+            ((System.ComponentModel.ISupportInitialize)(this.BackgroundImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Watermelon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Plum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Lemon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Grapes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Orange)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Cherry)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Seven)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Star)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox BackgroundImage;
+        private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.PictureBox Watermelon;
+        private System.Windows.Forms.PictureBox Plum;
+        private System.Windows.Forms.PictureBox Lemon;
+        private System.Windows.Forms.PictureBox Grapes;
+        private System.Windows.Forms.PictureBox Orange;
+        private System.Windows.Forms.PictureBox Cherry;
+        private System.Windows.Forms.PictureBox Seven;
+        private System.Windows.Forms.PictureBox Star;
+        private System.Windows.Forms.Label WatermelonLabel;
+        private System.Windows.Forms.Label PlumLabel;
+        private System.Windows.Forms.Label LemonLabel;
+        private System.Windows.Forms.Label SevenLabel;
+        private System.Windows.Forms.Label StarLabel;
+        private System.Windows.Forms.Label GrapesLabel;
+        private System.Windows.Forms.Label OrangeLabel;
+        private System.Windows.Forms.Label CherryLabel;
+    }
+}
